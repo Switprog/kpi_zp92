c7631e1b6a7f376d7ef557273cfa2d7b6e29a3f6
Switprog <marta1710@gmail.com>
2020-06-07 17:40:10 +0300

    lab5

---
+++ Lab5/Lab5/CashRegister.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class CashRegister
+    {
+        private Dictionary<string, int> goods;
+        private Dictionary<string, double> prices;
+
+        public CashRegister(Dictionary<string, double> prices)
+        {
+            this.prices = prices;
+            goods = new Dictionary<string, int>();
+        }
+
+        public Dictionary<string, int> Goods
+        {
+            get { return goods; }
+        }
+
+        public void AddGood(string name, int count)
+        {
+            if (goods.ContainsKey(name))
+                goods[name]++;
+            else
+                goods.Add(name, count);
+        }
+
+        public double GetPrice(string name)
+        {
+            return prices[name];
+        }
+
+        public string GetCheck()
+        {
+            SortedDictionary<string, int> sortedGoods = new SortedDictionary<string, int>(goods);
+            string check = "";
+            double total = 0;
+            foreach (string key in sortedGoods.Keys)
+            {
+                check += key + "   " + GetPrice(key) + " hrn  x" + goods[key]
+                    + "  " + goods[key] * GetPrice(key) + " hrn\n";
+                total += goods[key] * GetPrice(key);
+            }
+
+            return check + "Total sum = " + total + " hrn";
+
+        }
+    }
+}
---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+            dict.Add("Meat", 55.5);
+            dict.Add("Milk", 30.3);
+            dict.Add("Chocolate", 25.8);
+            dict.Add("Bread", 15.4);
+            CashRegister cashRegister = new CashRegister(dict);
+            cashRegister.AddGood("Chocolate", 2);
+            cashRegister.AddGood("Bread", 4);
+            cashRegister.AddGood("Meat", 5);
+            cashRegister.AddGood("Milk", 3);
+            cashRegister.AddGood("Chocolate", 2);
+            Console.WriteLine(cashRegister.GetCheck());
+            Console.ReadKey();
+        }
+    }
+}
