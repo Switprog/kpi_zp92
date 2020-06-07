a14384840ea52f3eabeff04200abdf548499c116
Switprog <marta1710@gmail.com>
2020-06-07 17:41:24 +0300

    lab6

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";
+
+            Coder coder = new Coder(new StreamWriter("buffer.txt"));
+            coder.Write(str);
+            coder.Close();
+            Console.WriteLine("Coded string: " + coder.GetString());
+            
+            Decoder decoder = new Decoder(new StreamReader("buffer.txt"));
+            Console.WriteLine("Decoded string: " + decoder.Read());
+            decoder.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
