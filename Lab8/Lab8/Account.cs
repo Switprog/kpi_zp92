c7d9d7854b74feb190eaad7fa1605eb46a72a6bf
Switprog <marta1710@gmail.com>
2020-06-07 17:44:22 +0300

    lab8

---
+++ Lab8/Lab8/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8
+{
+    public class Account
+    {
+        private int balance;
+        private string name;
+        private long id;
+
+
+        public Account(long id, string name)
+        {
+            this.name = name;
+            this.balance = 0;
+            this.id = id;
+        }
+
+        public int Balance
+        {
+            get { return balance; }
+        }
+
+        public long Id
+        {
+            get { return id; }
+        }
+
+        public void Recharge(int amount)
+        {
+            balance += amount;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Lab8/Lab8Tests/BankTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lab8;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab8Tests
+{
+    [TestClass]
+    public class BankTest
+    {
+        Bank bank;
+        int accountCount = (int)(new Random().NextDouble() * 2500);
+        int transactionCount = 100000 + ((int)new Random().NextDouble() * 25000);
+        BlockingCollection<Account> blockingCollection = new BlockingCollection<Account>();
+
+        [TestInitialize]
+        public void setUp()
+        {
+            bank = new Bank("PrivatBank");
+            for (int i = 0; i < accountCount; i++) {
+                bank.AddAccount("Svetlana");
+                foreach (Account account in bank.Accounts)
+                {
+                    account.Recharge((int)(new Random().NextDouble() * 1000));
+                }
+            }
+            Console.WriteLine("Total bank balance before transactions: " + bank.GetTotalBalance() + " $");
+        }
+
+        private async Task Transfer() {
+            List<Task> tasks = new List<Task>();
+            using (blockingCollection) {
+                for (int i = 0; i < transactionCount; i++)
+                {
+                    int sourceId = (int)(new Random().NextDouble() * accountCount);
+                    int destId = (int)(new Random().NextDouble() * accountCount);
+                    Task t = Task.Run(() =>
+                        bank.TransferMoney(bank.GetAccount(sourceId), bank.GetAccount(destId), (int)(new Random().NextDouble() * 10000)));
+                    tasks.Add(t);
+                }
+                {
+                    await Task.WhenAll(tasks);
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task TotalBalanceTest()
+        {
+            long expected = bank.GetTotalBalance();
+            await Transfer();
+            long actual = bank.GetTotalBalance();
+            Console.WriteLine("Total bank balance after transactions: " + actual + " $");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
