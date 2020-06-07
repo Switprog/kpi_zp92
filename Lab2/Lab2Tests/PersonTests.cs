a34a53f58f7c94348584ccc8a01788e8661c563b
Switprog <marta1710@gmail.com>
2020-06-07 17:36:37 +0300

    lab2

---
+++ Lab2/Lab2Tests/PersonTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Lab2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab2Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        private string fileName;
+        private Person person;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            fileName = "C:/Users/dafna/Desktop/Светлана/Prog/Lab2/Person.json";
+            person = new Person("Ivan", "Ivanovich", "Ivanovsky", 19, "male");
+            person.ToJson(fileName);
+        }
+
+        [TestMethod]
+        public void LoadFromJsonTest()
+        {
+            Person personFromJson = Person.FromJson(fileName);
+            Assert.IsTrue(person.Equals(personFromJson));
+        }
+    }
+}
