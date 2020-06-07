0ccbe4584b7bd0c8e61dc1ba431fec5f3112c94a
Switprog <marta1710@gmail.com>
2020-06-07 17:38:25 +0300

    lab3

---
+++ Lab3/Lab3/card/Card.cs
@@ -0,0 +1,53 @@
+﻿using Lab3.card_types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.card
+{
+    public abstract class Card
+    {
+        private long id;
+        private OwnerType ownerType;
+        private bool isBlocked;
+
+        public Card(long id, OwnerType ownerType)
+        {
+            this.id = id;
+            this.ownerType = ownerType;
+            isBlocked = false;
+
+        }
+
+        public long Id
+        {
+            get { return id; }
+        }
+
+        public OwnerType OwnerType
+        {
+            get { return ownerType; }
+        }
+
+        public bool IsBlocked
+        {
+            get { return isBlocked; }
+        }
+
+        public void Block()
+        {
+            isBlocked = true;
+        }
+
+        public string OwnerToString()
+        {
+            if (this.ownerType == OwnerType.ORDINARY) return "Ordinary";
+            else if (this.ownerType == OwnerType.PENSIONER) return "Pensioner";
+            else return "Student";
+        }
+        
+        public abstract bool Verify();
+    }
+}
---
+++ Lab3/Lab3/turnstile_system/Turnstile.cs
@@ -0,0 +1,49 @@
+﻿using Lab3.card;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.turnstile_system
+{
+    class Turnstile
+    {
+        private long id;
+        private Register register;
+
+        public Turnstile(long id, Register register)
+        {
+            this.id = id;
+            this.register = register;
+        }
+
+        private void blockIfExpired(Card card)
+        {
+            if (card.GetType() == typeof(QuantityCard) || card.GetType() == typeof(TermCard))
+                card.Block();
+        }
+
+        public string Warn(Card card)
+        {
+            return "Check documents if person is " + card.OwnerToString();
+        }
+
+        public bool Pass(Card card)
+        {
+            if (register.GetCardByUniqueId(card.Id) == null)
+            {
+                register.BlockCard(card);
+                return false;
+            }
+            bool pass = card.Verify();
+
+            if (!pass) blockIfExpired(card);
+
+            Console.WriteLine(Warn(card));
+
+            register.AddPass(new Pass(pass, id, card.Id));
+            return pass;
+        }
+    }
+}
