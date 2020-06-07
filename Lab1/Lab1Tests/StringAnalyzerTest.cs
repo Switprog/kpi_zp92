bc160957c09629639c3eb6ca8fc2cb4775857c2b
Switprog <marta1710@gmail.com>
2020-06-07 17:28:13 +0300

    lab1

---
+++ Lab1/Lab1Tests/StringAnalyzerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Lab1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab1Tests
+{
+    [TestClass]
+    public class StringAnalyzerTest
+    {
+        [TestMethod]
+        public void CountAverageLengthTest()
+        {
+            string[] input = { "word", "hello", "string" };
+            int expected = 5;
+            int actual = StringAnalyzer.CountAverageLength(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMoreThanAverageTest()
+        {
+            string[] input = { "word", "hello", "string" };
+            string[] expected = { "string" };
+            string[] actual = StringAnalyzer.FindMoreThanAverage(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
