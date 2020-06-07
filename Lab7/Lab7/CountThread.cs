aa8c704e249b4bb0ad76dcea841a6aaa8666a248
Switprog <marta1710@gmail.com>
2020-06-07 17:43:16 +0300

    lab7

---
+++ Lab7/Lab7/CountThread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class CountThread
+    {
+        public long passed;
+        private long iterationsPerProcess;
+        private Thread thread;
+
+        public CountThread(long iterationsPerProcess)
+        {
+            this.iterationsPerProcess = iterationsPerProcess;
+            thread = new Thread(new ThreadStart(this.RunThread));
+        }
+
+        public void Start() => thread.Start();
+        public void Join() => thread.Join();
+        public bool IsAlive => thread.IsAlive;
+
+
+        public void RunThread()
+        {
+            Random rnd = new Random();
+            double x, y;
+            double radius = 1.0;
+            passed = 0;
+            for (int i = 0; i < iterationsPerProcess; i++)
+            {
+                x = rnd.NextDouble();
+                y = rnd.NextDouble();
+                if ((x * x + y * y) < radius)
+                    passed++;
+            }
+        }
+    }
+}
